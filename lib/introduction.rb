946c36580e919ad905608717418deece4dcfa35b
Maxine <maxinemeurer@yahoo.com>
2021-02-01 16:10:16 -0500

    commit tree b7039725b7ebf98e008d9734abda586c91a12fba

---
+++ lib/introduction.rb
@@ -0,0 +1,8 @@
+#introduction #name
+def introduction(name)
+    puts "Hi, my name is #{name}."
+end
+#introduction #name #language
+def introduction_with_language(name, language)
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
